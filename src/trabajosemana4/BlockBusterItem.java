e29c5fdb4b82718b29f57f9b0cc1a84ffa0cb39b
damasofc <damasofc@hotmail.com>
2017-05-09 18:41:32 -0600

    clase BlockBusterItem terminada

--- src/trabajosemana4/BlockBusterItem.java
+++ src/trabajosemana4/BlockBusterItem.java
@@ -2,7 +2,25 @@
 
 package trabajosemana4;
 
+import java.util.Calendar;
 
-public class BlockBusterItem {
 
+public abstract class BlockBusterItem {
+    protected String nombre;
+    protected int codigo;
+    protected int precioRenta;
+    protected Calendar fecha = Calendar.getInstance();
+
+    public BlockBusterItem(String nombre, int codigo, int precioRenta) {
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.precioRenta = precioRenta;
+    }
+    
+    @Override
+    public String toString(){
+        return "Nombre: "+nombre +"\nCodigo: "+codigo+"\nPrecio de Renta: "+precioRenta;
+    }
+    
+    abstract double pagoRenta(int dias);
 }
